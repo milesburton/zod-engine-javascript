6c166da4aa1154e090d02053baddcb16a0924c35
miles <miles@mnetcs.com>
2012-09-02 17:15:02 +0100

    @Miles first commit of structure

---
+++ htdocs/js/map-loader.js
@@ -0,0 +1,14 @@
+(function (global) {
+
+
+
+
+
+
+
+    global.mapLoader = (global.module || {}).exports = mapLoader;
+    if (typeof module !== 'undefined') {
+        module.exports = mapLoader;
+    }
+
+})(this);
